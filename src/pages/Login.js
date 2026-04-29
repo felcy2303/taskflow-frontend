2c56bf02ec5902af1c9e480f8891cba254136f32
felcy2303 <felcyrovia@gmail.com>
2026-04-29 19:51:52 +0530

    Update login API endpoint to production URL

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,7 +20,7 @@ function Login() {
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "http://taskflow-backend-7xaw.onrender.com/api/auth/login",
         form
       );
 
